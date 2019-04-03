e0441032acbafb2e5edeba3bdfe4de544213c78a
Qianye Zhang <helloqyzhang@gmail.com>
2019-04-03 10:45:35 -0700

    modified gitignore file for deploy

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 //this two line are for socket.io
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-const config: SocketIoConfig = { url: 'http://192.168.1.49:8000', options: {} };
+const config: SocketIoConfig = { url: 'http://localhost:8000', options: {} };
 //
 import { NgModule } from '@angular/core';
 
